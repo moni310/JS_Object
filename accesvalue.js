62c3b5e965c0b5ca8cfff5e9a816a3289a89ac8f
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-23 14:06:36 +0530

    Add files via upload

---
+++ accesvalue.js
@@ -0,0 +1,12 @@
+var vegetables ={"veg1":"potato","veg2":"brinjal","veg3":"bottle gourd"};
+var s=vegetables.veg1;
+console.log(s)
+
+
+var myDetails={
+    "first name":"kumar",
+    "last name": "vadthya",
+    "middle name":"nayak"}
+
+console.log(myDetails["first name"])
+
