63c21c7e1bedc9c6b2bc83b8be6f6889a540caa5
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-23 14:02:47 +0530

    Add files via upload

---
+++ ques7.js
@@ -0,0 +1,8 @@
+var my_dict= { 1: 'NAVGURUKUL', 2: 'IN',3:{'A' : 'WELCOME', 'B' : 'To', 'C' : 'DHARAMSALA' }};
+for (i in my_dict){
+    if ((typeof my_dict[i])==="object"){
+        delete my_dict[i]["A"]
+    }
+    }
+console.log(my_dict)
+
